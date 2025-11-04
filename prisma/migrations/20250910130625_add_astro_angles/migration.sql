9aa9372d4a87a1370c9dbca64e64a2fadaf4e38c
sepic222 <saraellenpicard@icloud.com>
2025-11-04 22:41:15 +0100

    chore(prisma): init_postgres migration successfully applied

--- prisma/migrations/20250910130625_add_astro_angles/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Chart" ADD COLUMN "descendant" REAL;
-ALTER TABLE "Chart" ADD COLUMN "descendantSign" TEXT;
-ALTER TABLE "Chart" ADD COLUMN "ic" REAL;
-ALTER TABLE "Chart" ADD COLUMN "icSign" TEXT;
--- prisma/migrations/20251008124547_add_response_indexes/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "survey_responses_questionId_idx" ON "survey_responses"("questionId");
-
--- CreateIndex
-CREATE INDEX "survey_responses_submissionId_idx" ON "survey_responses"("submissionId");
